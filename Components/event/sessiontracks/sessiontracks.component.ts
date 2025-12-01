716bd546a77bb8c087b2f9336525bf3637b4eb5e
A Sravanthi <85950155+Sravanthi-11@users.noreply.github.com>
2025-12-02 01:04:39 +0530

    sT files

---
+++ Components/event/sessiontracks/sessiontracks.component.ts
@@ -0,0 +1,27 @@
+import { EventService } from '../../../services/event.service';
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { SessionTrack } from '../../../models/SessionTrack';
+
+@Component({
+    selector: 'app-sessiontracks',
+    templateUrl: './sessiontracks.component.html',
+    styleUrls: ['./sessiontracks.component.css'],
+    standalone: false
+})
+export class SessiontracksComponent implements OnInit {
+    @Input() readableEventId: string;
+    sessionTracks: SessionTrack[];
+
+    constructor(private eventService: EventService) {
+    }
+
+    ngOnInit() {
+        this.loadSessionTracks();
+    }
+
+    private loadSessionTracks(): void {
+        this.eventService.getSessionTracks(this.readableEventId).subscribe(tracks => {
+            this.sessionTracks = tracks;
+        }, error => console.error(error));
+    }
+}
